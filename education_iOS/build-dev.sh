88e1c2f7fcc1ae4693f5f497973b29bf84c7c9a9
SRS <sirusheng@agora.io>
2020-04-08 11:14:09 +0800

    update ci

--- education_iOS/build-dev.sh
+++ education_iOS/build-dev.sh
@@ -1,15 +1,23 @@
-#!/bin/sh
-ArchivePath=AgoraEducationDev.xcarchive
-IPAName="IPADEV"
+##!/bin/sh
+#ArchivePath=AgoraEducationDev.xcarchive
+#IPAName="IPADEV"
+#
+#xcodebuild clean -workspace "AgoraEducation.xcworkspace" -scheme "AgoraEducation" -configuration DevRelease
+#xcodebuild archive -workspace "AgoraEducation.xcworkspace" -scheme "AgoraEducation"  -configuration DevRelease -archivePath ${ArchivePath} -quiet || exit
+#xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath ${ArchivePath} -exportPath ${IPAName} -quiet || exit
+#cp ${IPAName}/AgoraEducation.ipa AgoraEducationDev.ipa
+echo "===>0"
+echo $0
+echo "===>1"
+echo $1
+echo "===>2"
+echo $2
+echo "===>3"
+echo $3
 
-xcodebuild clean -workspace "AgoraEducation.xcworkspace" -scheme "AgoraEducation" -configuration DevRelease
-xcodebuild archive -workspace "AgoraEducation.xcworkspace" -scheme "AgoraEducation"  -configuration DevRelease -archivePath ${ArchivePath} -quiet || exit
-xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath ${ArchivePath} -exportPath ${IPAName} -quiet || exit
-cp ${IPAName}/AgoraEducation.ipa AgoraEducationDev.ipa
-
-curl -X POST \
-https://upload.pgyer.com/apiv1/app/upload \
--H 'content-type: multipart/form-data' \
--F "uKey=$1" \
--F "_api_key=$2" \
--F  "file=@AgoraEducationDev.ipa"
+#curl -X POST \
+#https://upload.pgyer.com/apiv1/app/upload \
+#-H 'content-type: multipart/form-data' \
+#-F "uKey=$1" \
+#-F "_api_key=$2" \
+#-F  "file=@AgoraEducationDev.ipa"
