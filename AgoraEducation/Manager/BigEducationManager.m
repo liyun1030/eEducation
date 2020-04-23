9d3d0fcf9b85ac76e09ddd8e8b12e543ff36bea0
SRS <sirusheng@agora.io>
2020-04-23 14:36:55 +0800

    Squashed 'education_iOS/' content from commit 9feb5b3d
    git-subtree-dir: education_iOS
    git-subtree-split: 9feb5b3ddbb84ec60e9f90888c248dcb6b8c7aeb

---
+++ AgoraEducation/Manager/BigEducationManager.m
@@ -0,0 +1,261 @@
+//
+//  MinEducationManager.m
+//  AgoraEducation
+//
+//  Created by SRS on 2019/12/31.
+//  Copyright © 2019 Agora. All rights reserved.
+//
+
+#import "BigEducationManager.h"
+#import "JsonParseUtil.h"
+
+@interface BigEducationManager()
+
+
+@end
+
+@implementation BigEducationManager
+
+- (instancetype)init {
+    if(self = [super init]) {
+        self.renderStudentModels = [NSMutableArray array];
+        self.rtcUids = [NSMutableSet set];
+        self.rtcVideoSessionModels = [NSMutableArray array];
+    }
+    return self;
+}
+
+#pragma mark GlobalStates
+- (void)getRoomInfoCompleteSuccessBlock:(void (^ _Nullable) (RoomInfoModel * roomInfoModel))successBlock completeFailBlock:(void (^ _Nullable) (NSString *errMessage))failBlock {
+    
+    WEAK(self);
+    [super getRoomInfoCompleteSuccessBlock:^(RoomInfoModel * _Nonnull roomInfoModel) {
+        
+        weakself.renderStudentModels = [NSMutableArray array];
+        
+        weakself.roomModel = roomInfoModel.room;
+        weakself.studentModel = roomInfoModel.localUser;
+        
+        if(weakself.roomModel != nil && weakself.roomModel.coVideoUsers != nil) {
+            for(UserModel *userModel in weakself.roomModel.coVideoUsers) {
+               if(userModel.role == UserRoleTypeTeacher) {
+                   weakself.teacherModel = userModel;
+               } else if(userModel.role == UserRoleTypeStudent) {
+                   [weakself.renderStudentModels addObject:[userModel yy_modelCopy]];
+               }
+            }
+        }
+
+        if(successBlock != nil) {
+            successBlock(roomInfoModel);
+        }
+    } completeFailBlock:failBlock];
+}
+- (void)updateEnableChatWithValue:(BOOL)enableChat completeSuccessBlock:(void (^ _Nullable) (void))successBlock completeFailBlock:(void (^ _Nullable) (NSString *errMessage))failBlock {
+    
+    WEAK(self);
+    [super updateEnableChatWithValue:enableChat completeSuccessBlock:^{
+        weakself.studentModel.enableChat = enableChat;
+        for (UserModel *model in weakself.renderStudentModels) {
+            if(model.uid == weakself.studentModel.uid){
+                model.enableChat = enableChat;
+                break;
+            }
+        }
+        
+        if(successBlock != nil) {
+            successBlock();
+        }
+    } completeFailBlock:failBlock];
+}
+- (void)updateEnableVideoWithValue:(BOOL)enableVideo completeSuccessBlock:(void (^ _Nullable) (void))successBlock completeFailBlock:(void (^ _Nullable) (NSString *errMessage))failBlock {
+    
+    WEAK(self);
+    [super updateEnableVideoWithValue:enableVideo completeSuccessBlock:^{
+        weakself.studentModel.enableVideo = enableVideo;
+        for (UserModel *model in weakself.renderStudentModels) {
+            if(model.uid == weakself.studentModel.uid){
+                model.enableVideo = enableVideo;
+                break;
+            }
+        }
+        
+        if(successBlock != nil) {
+            successBlock();
+        }
+    } completeFailBlock:failBlock];
+}
+- (void)updateEnableAudioWithValue:(BOOL)enableAudio completeSuccessBlock:(void (^ _Nullable) (void))successBlock completeFailBlock:(void (^ _Nullable) (NSString *errMessage))failBlock {
+    
+    WEAK(self);
+    [super updateEnableAudioWithValue:enableAudio completeSuccessBlock:^{
+        weakself.studentModel.enableAudio = enableAudio;
+        for (UserModel *model in weakself.renderStudentModels) {
+            if(model.uid == weakself.studentModel.uid){
+                model.enableAudio = enableAudio;
+                break;
+            }
+        }
+        
+        if(successBlock != nil) {
+            successBlock();
+        }
+    } completeFailBlock:failBlock];
+}
+
+- (void)updateLinkStateWithValue:(BOOL)coVideo completeSuccessBlock:(void (^ _Nullable) (void))successBlock completeFailBlock:(void (^ _Nullable) (NSString *errMessage))failBlock {
+    
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"coVideo"] = @(coVideo ? 1 : 0);
+    
+    WEAK(self);
+    [self updateUserInfoWithParams:params completeSuccessBlock:^{
+        
+        weakself.studentModel.coVideo = coVideo;
+        for (UserModel *model in weakself.renderStudentModels) {
+            if(model.uid == weakself.studentModel.uid){
+                model.coVideo = coVideo;
+                break;
+            }
+        }
+        
+        if(successBlock != nil) {
+            successBlock();
+        }
+        
+    } completeFailBlock:failBlock];
+    [self updateUserInfoWithParams:params completeSuccessBlock:successBlock completeFailBlock:failBlock];
+}
+
+#pragma mark Signal
+- (void)sendPeerSignalWithModel:(SignalP2PCmdType)type completeSuccessBlock:(void (^ _Nullable) (void))successBlock completeFailBlock:(void (^ _Nullable) (NSInteger errorCode))failBlock {
+    
+    NSString *msgText = @"";
+    switch (type) {
+        case SignalP2PCmdTypeApply: {
+            NSDictionary *dataDic = @{@"userId" : self.studentModel.userId,
+                                      @"account" : self.studentModel.userName,
+                                      @"operate" : @(SignalP2PCmdTypeApply)};
+            
+            NSDictionary *dict = @{@"cmd" : @(SignalP2PTypeHand),
+                                   @"data" : dataDic};
+            msgText = [JsonParseUtil dictionaryToJson:dict];
+        }
+            break;
+        default:
+            break;
+    }
+    
+    if (msgText.length == 0 || self.teacherModel == nil) {
+        return;
+    }
+    NSString *peerId = @(self.teacherModel.uid).stringValue;
+    
+    [self.signalManager sendMessage:msgText toPeer:peerId completeSuccessBlock:^{
+        if(successBlock != nil) {
+            successBlock();
+        }
+    } completeFailBlock:^(NSInteger errorCode) {
+        if(failBlock != nil) {
+            failBlock(errorCode);
+        }
+    }];
+}
+
+#pragma mark RTC
+- (void)setupRTCVideoCanvas:(RTCVideoCanvasModel *)model completeBlock:(void(^ _Nullable)(AgoraRtcVideoCanvas *videoCanvas))block {
+    
+    RTCVideoSessionModel *currentSessionModel;
+    RTCVideoSessionModel *removeSessionModel;
+    for (RTCVideoSessionModel *videoSessionModel in self.rtcVideoSessionModels) {
+        // view rerender
+        if(videoSessionModel.videoCanvas.view == model.videoView){
+            videoSessionModel.videoCanvas.view = nil;
+            if(videoSessionModel.uid == self.signalManager.messageModel.uid.integerValue) {
+                [self.rtcManager setupLocalVideo:videoSessionModel.videoCanvas];
+            } else {
+                [self.rtcManager setupRemoteVideo:videoSessionModel.videoCanvas];
+            }
+            removeSessionModel = videoSessionModel;
+
+        } else if(videoSessionModel.uid == model.uid){
+            videoSessionModel.videoCanvas.view = nil;
+            if(videoSessionModel.uid == self.signalManager.messageModel.uid.integerValue) {
+                [self.rtcManager setupLocalVideo:videoSessionModel.videoCanvas];
+            } else {
+                [self.rtcManager setupRemoteVideo:videoSessionModel.videoCanvas];
+            }
+            currentSessionModel = videoSessionModel;
+        }
+    }
+    
+    WEAK(self);
+    [super setupRTCVideoCanvas:model completeBlock:^(AgoraRtcVideoCanvas *videoCanvas) {
+        
+        if(removeSessionModel != nil){
+            AgoraLogInfo(@"VideoSessionModels remove repeat view uid:%lu", (unsigned long)removeSessionModel.uid);
+            [weakself.rtcVideoSessionModels removeObject:removeSessionModel];
+        }
+        if(currentSessionModel != nil){
+            AgoraLogInfo(@"VideoSessionModels remove repeat uid:%lu", (unsigned long)currentSessionModel.uid);
+            [weakself.rtcVideoSessionModels removeObject:currentSessionModel];
+        }
+        
+        AgoraLogInfo(@"VideoSessionModels add:%lu", (unsigned long)model.uid);
+        
+        RTCVideoSessionModel *videoSessionModel = [RTCVideoSessionModel new];
+        videoSessionModel.uid = model.uid;
+        videoSessionModel.videoCanvas = videoCanvas;
+        [weakself.rtcVideoSessionModels addObject:videoSessionModel];
+        
+        if(block != nil){
+            block(videoCanvas);
+        }
+    }];
+}
+
+- (void)removeRTCVideoCanvas:(NSUInteger) uid {
+
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"uid == %d", uid];
+    NSArray<RTCVideoSessionModel *> *filteredArray = [self.rtcVideoSessionModels filteredArrayUsingPredicate:predicate];
+    if(filteredArray > 0) {
+        RTCVideoSessionModel *model = filteredArray.firstObject;
+        model.videoCanvas.view = nil;
+        if(uid == self.signalManager.messageModel.uid.integerValue) {
+            [self.rtcManager setupLocalVideo:model.videoCanvas];
+        } else {
+            [self.rtcManager setupRemoteVideo:model.videoCanvas];
+        }
+        [self.rtcVideoSessionModels removeObject:model];
+        
+        AgoraLogInfo(@"VideoSessionModels remove given uid:%lu", (unsigned long)model.uid);
+    }
+}
+
+- (void)releaseResources {
+
+    for (RTCVideoSessionModel *model in self.rtcVideoSessionModels){
+        model.videoCanvas.view = nil;
+        
+        if(model.uid == self.signalManager.messageModel.uid.integerValue) {
+            [self.rtcManager setupLocalVideo:model.videoCanvas];
+        } else {
+            [self.rtcManager setupRemoteVideo:model.videoCanvas];
+        }
+    }
+    [self.rtcVideoSessionModels removeAllObjects];
+
+    // release rtc
+    [self releaseRTCResources];
+    
+    // release white
+    [self releaseWhiteResources];
+    
+    // release signal
+    [self releaseSignalResources];
+    
+    [BaseEducationManager leftRoomWithSuccessBolck:nil completeFailBlock:nil];
+
+}
+
+@end
---
+++ AgoraEducation/Manager/EduModel/EduConfigModel.m
@@ -0,0 +1,85 @@
+//
+//  EduConfigModel.m
+//  AgoraEducation
+//
+//  Created by SRS on 2020/1/21.
+//  Copyright © 2019 Agora. All rights reserved.
+//
+
+#import "EduConfigModel.h"
+#import "URL.h"
+
+static EduConfigModel *manager = nil;
+
+@implementation EduConfigModel
+
++ (instancetype)shareInstance {
+    if(!manager){
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            manager = [EduConfigModel new];
+        });
+    }
+    return manager;
+}
+
+// 防止使用alloc开辟空间
++ (instancetype)allocWithZone:(struct _NSZone *)zone{
+    if(!manager){
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            manager = [super allocWithZone:zone];
+        });
+    }
+    return manager;
+}
+
+- (instancetype)init{
+    @synchronized(self) {
+        if(self = [super init]) {
+            self.httpBaseURL = HTTP_BASE_URL;
+        }
+    }
+    return self;
+}
+
+- (void)setHttpBaseURL:(NSString *)url {
+    if(url != nil && url.length > 0) {
+        _httpBaseURL = url;
+        NSString *lastString = [url substringFromIndex:url.length-1];
+        if([lastString isEqualToString:@"/"]) {
+            _httpBaseURL = [url substringWithRange:NSMakeRange(0, [url length] - 1)];
+        }
+    } else {
+        self.httpBaseURL = HTTP_BASE_URL;
+    }
+}
+
+#pragma mark Private
++ (NSString *)generateHttpErrorMessageWithDescribe:(NSString *)des errorCode:(NSInteger)errorCode {
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSArray<NSString*> *allLanguages = [defaults objectForKey:@"AppleLanguages"];
+    NSString *preferredLang = [allLanguages objectAtIndex:0];
+    NSString *msg = @"";
+    if([preferredLang containsString:@"zh-Hans"]) {
+        msg = [EduConfigModel.shareInstance.multiLanguage.cn valueForKey:@(errorCode).stringValue];
+    } else {
+        msg = [EduConfigModel.shareInstance.multiLanguage.en valueForKey:@(errorCode).stringValue];
+    }
+    
+    if(msg == nil || msg.length == 0) {
+        msg = [NSString stringWithFormat:@"%@：%ld", des, (long)errorCode];
+    }
+    return msg;
+}
+
++ (NSDictionary *)generateHttpAuthHeader {
+    NSMutableDictionary *headers = [NSMutableDictionary dictionary];
+    if(EduConfigModel.shareInstance.rtmToken != nil && EduConfigModel.shareInstance.rtmToken.length > 0 && EduConfigModel.shareInstance.uid > 0) {
+        headers[@"x-agora-token"] = EduConfigModel.shareInstance.rtmToken;
+        headers[@"x-agora-uid"] = @(EduConfigModel.shareInstance.uid).stringValue;
+    }
+    return headers;
+}
+@end
