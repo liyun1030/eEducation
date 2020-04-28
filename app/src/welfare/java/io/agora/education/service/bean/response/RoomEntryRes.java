c10264cf2e50b8617593a818b82a7ef6c3605483
HUI <15215604969@163.com>
2020-04-28 16:28:51 +0800

    Squashed 'education_Android/AgoraEducation/' changes from c3b239e..8fcec17
    8fcec17 fix layout
    3308ce7 Merge branch 'dev/5.4.0' into dev/5.4.0-welfare
    5d2c2c8 fix bugs
    a5f75c7 Merge branch 'dev/5.4.0' into dev/5.4.0-welfare
    d25e904 add follow tips
    4fe44d0 add log to demo
    f61df37 Merge branch 'dev/5.4.0' into dev/5.4.0-welfare
    1984dfa optimize
    dae3666 optimize
    33fd730 Merge branch 'dev/5.4.0' into dev/5.4.0-welfare
    2b79d58 optimize
    7a26583 finish
    a2af384 Merge branch 'dev/5.4.0' into dev/5.4.0-welfare
    a90eec9 finish
    ab5ee84 uuid to userUuid
    6133d8f finish
    3d3541d Merge branch 'dev/5.3.1' into dev/5.3.1-welfare
    41372a0 fix
    c539ff3 init
    ebf6ce7 upgrade version
    1e20d39 upgrade version
    920153d Merge branch 'dev/5.2.1' into dev/5.2.1-welfare
    de3f9f5 finish
    112fcbe finish
    ee72173 Merge branch 'dev/5.2.0' into dev/welfare
    b4aa555 fix screen share bug
    871764a Merge branch 'dev/5.2.0' into dev/welfare
    8272384 Merge branch 'master' into dev/5.2.0
    021578c Merge branch 'dev/5.2.0' into dev/welfare
    bfa3b03 add ci
    2cf59ba Merge branch 'dev/5.2.0' into dev/welfare
    460f332 fix time
    6de0239 Merge branch 'dev/5.2.0' into dev/welfare
    5920f4d add record status
    05de7b3 fix bug
    d09481b Merge branch 'dev/5.2.0' into dev/welfare
    7006158 fix bug
    76e12a2 Merge branch 'welfare' into dev/welfare
    1e93d5e Merge branch 'master' into dev/5.2.0
    3f7fe11 Merge pull request #2 from AgoraIO-Community/dev/welfare_language
    4e281ca Merge branch 'dev/language' into dev/welfare_language
    33de58a Merge branch 'dev/language' into dev/welfare_usa
    4cc8688 optimize translate
    a2b1bf6 Merge branch 'dev/welfare_usa' into dev/welfare
    20c30a0 Merge branch 'dev/5.2.0' into dev/welfare
    87a7fde multi language
    1942a62 Merge branch 'dev/language' into dev/welfare_usa
    6ee408e multi language
    dd11491 Merge branch 'dev/language' into dev/5.2.0
    953d602 change wuhan to welfare
    5e45c76 change wuhan to welfare
    40cb33d dev finish
    308f028 upgrade bean
    f325a00 remove board sdk token
    a7d09b0 Merge branch '5.2.0' into wuhan
    45eecde remove board sdk token
    d8c32b1 fix bug
    fa0d099 remove unless code
    28ba85a Merge branch '5.2.0' into wuhan
    42b7680 config && login
    e6ebb70 Merge branch '5.2.0' into wuhan
    4fc4118 init
    2f6d8d5 upgrade to 5.1.0
    a4476ec finish timeline
    11373d6 Merge branch 'Android/5.1.0' into Android/wuhan
    9a91c14 add english app name
    80e10e0 Merge branch 'Android/5.1.0' into Android/wuhan
    32bd4b2 Merge branch 'Android/5.1.0' into Android/wuhan
    a966911 Merge branch 'Android/5.1.0' into Android/wuhan
    bb281f1 init
    
    git-subtree-dir: education_Android/AgoraEducation
    git-subtree-split: 8fcec175ab2200960b7d2cb3d86e0f852df0e136

---
+++ app/src/welfare/java/io/agora/education/service/bean/response/RoomEntryRes.java
@@ -0,0 +1,11 @@
+package io.agora.education.service.bean.response;
+
+import io.agora.education.service.bean.Room;
+import io.agora.education.service.bean.User;
+
+public class RoomEntryRes {
+
+    public Room room;
+    public User user;
+
+}
